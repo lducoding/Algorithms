47eb94afc8f651ad38c1c4f8dc933a2c5a4644a5
ldu <lducoding@gmail.com>
2021-12-23 17:57:41 +0900

    장르별 인기곡 2개뽑아야함

--- src/Hash/BestAlbum.java
+++ src/Hash/BestAlbum.java
@@ -1,52 +1,63 @@
 package Hash;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class BestAlbum {
     public static void main(String[] args) {
-        String genre[] = {"classic", "pop", "classic", "classic", "pop"};
-        int plays[] = {500, 600, 150, 800, 2500};
+        String genre[] = {"classic", "pop", "classic", "pop", "classic", "classic"};
+        int plays[] = {400, 600, 150, 2500, 500, 500};
         System.out.println(solution(genre,plays));
     }
 
     public static int[] solution(String[] genres, int[] plays) {
+
         int[] answer = {};
         HashMap<String,Integer> map = new HashMap<String,Integer>();
-        ArrayList<String> rankList = new ArrayList<String>();
-        ArrayList<Integer> list = new ArrayList<Integer>();
 
         for(int i = 0; i < genres.length; i++) {
             map.put(genres[i],map.getOrDefault(genres[i],0)+plays[i]);
         }
-        Iterator<Integer> it = map.values().iterator();
-        while (it.hasNext()) {
-            list.add(it.next().intValue());
-        }
-        Collections.sort(list,Collections.reverseOrder());
 
-        for (int val : list) {
-            String keyList =  getKey(map, val);
-            rankList.add(keyList);
-        }
-
-        for(String rank : rankList) {
-            for (int i = 0; i < genres.length; i++) {
-
+        ArrayList<String> rankList = new ArrayList<>();
+        while(map.size()!=0) {
+            String max_gen = "";
+            int max_play = -1;
+            for (String gen : map.keySet()) {
+                if(map.get(gen) > max_play) {
+                    max_gen = gen;
+                    max_play = map.get(gen);
+                }
             }
+            rankList.add(max_gen);
+            map.remove(max_gen);
         }
         System.out.println(rankList);
+
+        ArrayList<BestSong> resultList = new ArrayList<>();
+        for (String gen : rankList) {
+            ArrayList<BestSong> semiResult = new ArrayList<>();
+            for (int i = 0; i < genres.length; i++) {
+                if(gen.equals(genres[i])) {
+                    semiResult.add(new BestSong(genres[i],plays[i],i));
+                }
+            }
+            
+        }
 
         return answer;
     }
 
-    public static <K, V> K getKey(Map<K, V> map, V value) {
+}
+class BestSong {
+        String gen = "";
+        int play = 0;
+        int idx = 0;
 
-        for (K key : map.keySet()) {
-            if (value.equals(map.get(key))) {
-                return key;
-            }
+        public BestSong(String gen, int play, int idx) {
+            this.gen = gen;
+            this.play = play;
+            this.idx = idx;
         }
-        return null;
-    }
 }
