eddce7265bb3450874ac790b6817d1edb5d2b541
lducoding <lducoding@gmail.com>
2021-12-30 21:57:58 +0900

    트럭다리 푸는중

--- src/Stack_Queue/TruckOnBridge.java
+++ src/Stack_Queue/TruckOnBridge.java
@@ -1,15 +1,32 @@
 package Stack_Queue;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TruckOnBridge {
     public static void main(String[] args) {
         int bridge_length = 2;
         int weight = 10;
-        int[] truck_weight = {7,4,6,8};
+        int[] truck_weight = {7,4,5,6};
         System.out.println(solution(bridge_length,weight,truck_weight));
     }
 
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        Arrays.sort(truck_weights);
+
+        int size = truck_weights.length-1;
+
+        for (int i = size; i >= 0; i--) {
+            queue.offer(truck_weights[i]);
+        }
+        System.out.println(queue.poll());
+
         return answer;
     }
 }
