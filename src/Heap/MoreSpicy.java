cc81eccb2eeb8eeefaf884659ff87b6247860512
lducoding <lducoding@gmail.com>
2022-01-12 21:56:32 +0900

    더 맵게 priorityQueue 사용해서 해결

--- src/Heap/MoreSpicy.java
+++ src/Heap/MoreSpicy.java
@@ -3,42 +3,32 @@ package Heap;
 import sun.lwawt.macosx.CSystemTray;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class MoreSpicy {
     public static void main(String[] args) {
-        int[] scoville = {13,3,2,9,10,12};
+        int[] scoville = {1, 2, 3, 9, 10, 12};
         int k = 7;
         System.out.println(solution(scoville,k));
     }
 
     public static int solution(int[] scoville, int K) {
         int answer = 0;
+        PriorityQueue<Integer> heap =  new PriorityQueue<>();
 
-        for (int i = 1; i < scoville.length; i++) {
-            int root = 0;
-            int c = i;
-            int node1 = 0;
-            while (c!=0) {
-                root = (c-1) / 2;
-                node1 = 2 * root + 1;
-                if(scoville[root] > scoville[i]) {
-                    int temp = scoville[root];
-                    scoville[root] = scoville[i];
-                    scoville[i] = temp;
-                }
-                if(node1 < scoville.length-1) {
-                    if(scoville[node1] > scoville[node1+1]) {
-                        int temp = scoville[node1];
-                        scoville[node1] = scoville[node1+1];
-                        scoville[node1+1] = temp;
-                    }
-                }
-                c = root;
-            }
-
+        for(int scov : scoville) {
+            heap.offer(scov);
         }
-        System.out.println("1차:"+Arrays.toString(scoville));
+        while(heap.peek() < K) {
+            if(heap.size() ==1) {
+                return -1;
+            }
+            int min = heap.poll();
+            int min2 = heap.poll();
 
+            heap.offer(min + ( min2 *2 ));
+            answer++;
+        }
         return answer;
     }
 }
