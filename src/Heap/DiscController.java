2d95650fcaac8121d9c11b8cace3ca123d22c2a8
lducoding <lducoding@gmail.com>
2022-01-14 21:45:31 +0900

    풀이 했지만 시간초과 되고 틀림

--- src/Heap/DiscController.java
+++ src/Heap/DiscController.java
@@ -12,17 +12,26 @@ public class DiscController {
     public static int solution(int[][] jobs) {
         int answer = 0;
         int time = 0;
+        // 시간 순서로 정렬
         Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
+        // 작업 순서로 담음
         PriorityQueue<int[]> heap = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
         time += jobs[0][1];
+        answer = time - jobs[0][0];
         for (int i = 1; i < jobs.length; i++) {
             heap.add(jobs[i]);
         }
         while (true) {
             if(time >= heap.peek()[0]) {
-                break;
+                System.out.println("실행:"+heap.peek()[1]);
+                int temp = heap.peek()[0];
+                time += heap.poll()[1];
+                answer += time - temp;
+                if(heap.size()==0) {
+                    break;
+                }
             }
         }
-        return answer;
+        return answer/ jobs.length;
     }
 }
