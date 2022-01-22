172f9578df0b9eacdd6d957539299e4072c63be5
lducoding <lducoding@gmail.com>
2022-01-22 12:14:23 +0900

    재귀 공부중

--- src/FullSearch/FindSosu.java
+++ src/FullSearch/FindSosu.java
@@ -1,6 +1,7 @@
 package FullSearch;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class FindSosu {
     public static void main(String[] args) {
@@ -8,11 +9,33 @@ public class FindSosu {
         //String numbers = "011";
         System.out.println(solution("17"));
     }
-
+    public static boolean isPrime(int ckNum) {
+        if(ckNum == 0 || ckNum == 1) {
+            return false;
+        }
+        int limit = (int)Math.sqrt(ckNum);
+        for (int i = 2; i <= limit; i++) {
+            if(ckNum % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     static HashSet<Integer> set = new HashSet<>();
     public static int solution(String numbers) {
         int answer = 0;
         recursive("", numbers);
+        System.out.println(set);
+
+        Iterator<Integer> it = set.iterator();
+
+        while (it.hasNext()) {
+            int num = it.next();
+            if(isPrime(num)) {
+                answer++;
+            }
+        }
+
         return answer;
     }
 
