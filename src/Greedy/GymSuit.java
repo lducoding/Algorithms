2636d2290c96dc44b938c730891662e75354b684
lducoding <lducoding@gmail.com>
2022-01-25 20:33:20 +0900

    체육복

--- src/Greedy/GymSuit.java
+++ src/Greedy/GymSuit.java
@@ -1,32 +1,40 @@
 package Greedy;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class GymSuit {
     public static void main(String[] args) {
-        int n = 5;
-        int[] lost = {3,4};
-        int[] reserve = {1,3,5};
+        int n = 7;
+        int[] lost = {1,2,4,5};
+        int[] reserve = {2,3,5};
         System.out.println(solution(n,lost,reserve));
     }
 
     public static int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-
         n -= lost.length;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
 
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < reserve.length; i++) {
-            queue.add(reserve[i]);
+
+        for (int i = 0; i < lost.length; i++) {
+            if (Arrays.binarySearch(reserve, lost[i]) >= 0) {
+                reserve[Arrays.binarySearch(reserve, lost[i])] = -1;
+                lost[i] = -1;
+                n++;
+            }
         }
 
         for (int i = 0; i < lost.length; i++) {
-            if(!queue.isEmpty()) {
-                int num = queue.peek();
-                if(lost[i]-1 == num || lost[i]+1 == num) {
-                    queue.poll();
-                    n++;
+            if(lost[i] != -1) {
+                for (int j = 0; j < reserve.length; j++) {
+                    if(lost[i]-1 == reserve[j] || lost[i]+1 == reserve[j] ) {
+                        reserve[j] = -1;
+                        lost[i] = -1;
+                        n++;
+                    }
                 }
             }
         }
