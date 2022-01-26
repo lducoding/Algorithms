63627392194c09a022640941db7560dce17837d3
lducoding <lducoding@gmail.com>
2022-01-26 21:54:06 +0900

    체육복 효율성있게 품

--- src/Greedy/GymSuit.java
+++ src/Greedy/GymSuit.java
@@ -14,31 +14,30 @@ public class GymSuit {
     }
 
     public static int solution(int n, int[] lost, int[] reserve) {
-        n -= lost.length;
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        int[] student = new int[n];
+        int answer = n;
 
-
-        for (int i = 0; i < lost.length; i++) {
-            if (Arrays.binarySearch(reserve, lost[i]) >= 0) {
-                reserve[Arrays.binarySearch(reserve, lost[i])] = -1;
-                lost[i] = -1;
-                n++;
-            }
+        for (int l : lost) {
+            student[l-1]--;
+        }
+        for (int r : reserve) {
+            student[r-1]++;
         }
 
-        for (int i = 0; i < lost.length; i++) {
-            if(lost[i] != -1) {
-                for (int j = 0; j < reserve.length; j++) {
-                    if(lost[i]-1 == reserve[j] || lost[i]+1 == reserve[j] ) {
-                        reserve[j] = -1;
-                        lost[i] = -1;
-                        n++;
-                    }
+        for (int i = 0; i < student.length; i++) {
+            if(student[i] == -1) {
+                if(i-1>=0 && student[i-1] == 1) {
+                    student[i]++;
+                    student[i-1]--;
+                } else if(i+1<student.length && student[i+1] == 1) {
+                    student[i]++;
+                    student[i+1]++;
+                } else {
+                    answer--;
                 }
             }
         }
 
-        return n;
+        return answer;
     }
 }
