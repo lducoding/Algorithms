8cd0c6b92051f869c82247b3572f2a74f1ea26e1
lducoding <lducoding@gmail.com>
2022-01-28 17:32:36 +0900

    조이스틱 정답 보고 이해

--- src/Greedy/Joystick.java
+++ src/Greedy/Joystick.java
@@ -4,15 +4,27 @@ public class Joystick {
     public static void main(String[] args) {
         String name = "JAN";
         System.out.println(solution(name));
+        // a b c d e f g h i j k l m n o p q r s t u v w x y z
+        // 1 2 3 4 5 6 7 8 9 1011121314151617181920212223242526
     }
 
     public static int solution(String name) {
         int answer = 0;
+        int[] diff={0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1};
+        for(char c:name.toCharArray())
+            answer+=diff[c-'A'];
 
-        for (int i = 0; i < name.length(); i++) {
-            System.out.println((int)name.charAt(i)-64);
+        int length=name.length();
+        int min=length-1;
+
+        for(int i=0;i<length;i++){
+            int next=i+1;
+            while(next<length && name.charAt(next)=='A'){
+                next++;
+            }
+            min=Math.min(min,i+length-next+Math.min(i,length-next));
         }
 
-        return answer;
+        return answer+min;
     }
 }
