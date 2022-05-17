ee44b1142e0d54a5802a74fead83f2ea8ee7d2c6
root <admin@example.com>
2022-05-17 21:19:41 +0900

    준비단계

--- src/Greedy/LifeBoat.java
+++ src/Greedy/LifeBoat.java
@@ -2,10 +2,12 @@ package Greedy;
 
 public class LifeBoat {
     public static void main(String[] args) {
-        
+        int[] people = {1,2,3,};
+        int limit = 50;
+        System.out.println(solution(people, limit));
     }
 
-    public int solution(int[] people, int limit) {
+    public static int solution(int[] people, int limit) {
         int answer = 0;
         return answer;
     }
