4960cc8e18d33ce471281ab9e042fa9077a68f73
root <admin@example.com>
2022-05-17 21:15:24 +0900

    구명보트 시작

---
+++ src/Greedy/LifeBoat.java
@@ -0,0 +1,12 @@
+package Greedy;
+
+public class LifeBoat {
+    public static void main(String[] args) {
+        
+    }
+
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        return answer;
+    }
+}
